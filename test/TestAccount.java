20c7b1d080c571791a3bf419afe347905dc9e807
wholagan <ruscav@gmail.com>
2019-03-09 16:01:13 -0800

    test file

---
+++ test/TestAccount.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestAccount {
+    Address testAddress;
+
+    @Before
+    public void setUp() throws Exception {
+        testAddress = new Address("123 Easy St.", "Eureka",
+                "California", "94599");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println(testAddress.toString());
+    }
+}
