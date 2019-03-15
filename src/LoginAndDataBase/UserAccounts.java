85225c36d0f3d82dd3b44f71fdb9b275bba7c809
Daniel G <48115298+dan8kfm@users.noreply.github.com>
2019-03-14 19:04:28 -0700

    Patch for the LoadAndDataBase classes.

--- src/LoginAndDataBase/UserAccounts.java
+++ src/LoginAndDataBase/UserAccounts.java
@@ -1,52 +1,59 @@
 package LoginAndDataBase;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.Scanner;
 
 public class UserAccounts implements Serializable {
     private String username;
     private String password;
-
+    // Constructor
     public UserAccounts(){
         this.username = "";
         this.password = "";
     }
-
     public UserAccounts(String username, String password){
         this.username = username;
         this.password = password;
     }
 
     // Setters
-    private void setUsername(String user) { username = user; }
-    private void setPassword(String pass) { password = pass; }
+    public void setUsername(String user) { username = user; }
+    public void setPassword(String pass) { password = pass; }
 
-    // Getters
+    /*Promps the user to input their login name and its checked in the validLogin function.*/
     public String getUsername() {
         Scanner testUser = new Scanner(System.in);
         System.out.println("Username: ");
         username = testUser.next();
         return username;
     }
+    /*Promps the user to input their password and its checked in the validLogin function.*/
     public String getPassword() {
         Scanner testPass = new Scanner(System.in);
         System.out.println("Password: ");
         password = testPass.nextLine();
         return password;
     }
-
-    public void addUser()
-    {
+    /*This adds a new user to the useraccount text file.
+    * This will also create a file with them that will automatically load when they
+    * login. */
+    public void addUser() throws IOException {
+        DataBase newFile = new DataBase();
         Scanner input = new Scanner(System.in);
         System.out.println("Please Enter Your Desired Username: ");
         String setUser = input.nextLine();
-
         System.out.println("Please Enter Your Desired Password: ");
         String setPass = input.nextLine();
 
         setUsername(setUser);
         setPassword(setPass);
-        // should save to useraccount.txt when made
+        File uaFile = new File("src/database/useraccount.txt").getAbsoluteFile();
+        BufferedWriter saveUser = new BufferedWriter(new FileWriter(uaFile,true));
+        String save = setUser +":"+setPass+"\r\n";
+        saveUser.write(save);
+        saveUser.close();
+        newFile.loadUserAccounts(setUser);
+        System.out.println("User Created");
     }
 
 }
