cce3041f85b92bc31c369350c8d91739ba3ec7f7
Daniel G <48115298+dan8kfm@users.noreply.github.com>
2019-03-27 09:46:39 -0700

    taco

---
+++ app/src/main/java/cs401/LoginAndDataBase/LoginGUI/LoginActivity.java
@@ -0,0 +1,15 @@
+package cs401.LoginAndDataBase.LoginGUI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cs401.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
